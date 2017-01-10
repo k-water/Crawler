34a6fbf9ef970ab01a360a66cf078da414accf16
k-water <625592890@qq.com>
2017-01-10 15:16:15 +0800

    feat：eslint校验&爬取文章存储到本地&http服务器对结果展示

---
+++ app/index.js
@@ -0,0 +1,9 @@
+/**
+ * @author water
+ * @date 2017-01-09 17:12:21
+ * @description A try for node
+ */
+var collector = require('./collector.js')
+var url = 'http://blog.lxstart.net/2016/08/04/javascript/pattern/singleton/'
+
+collector.fetchPage(url)
